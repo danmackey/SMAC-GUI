80f9f5514cbae730a766ae65f1ce6b35fb2c1a9d
Danny Mackey <danmack768@gmail.com>
2021-01-16 01:14:34 -0500

    Moved to .NET 5. Refactored MainWindow.xaml.cs to leverage properties.  Added XAML Styler to improve readability of xaml files.

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -9,29 +9,15 @@ namespace SMAC
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
         private readonly Controller controller;
-        private string _currentServerMessage;
-        public string CurrentServerMessage
-        {
-            get
-            {
-                return _currentServerMessage;
-            }
-            set
-            {
-                if (value != _currentServerMessage)
-                {
-                    _currentServerMessage = value;
-                    OnPropertyChanged("CurrentServerMessage");
-                }
-            }
-        }
+        public bool IsSimMonstersApiServer => controller.settings.smApiServerActive;
+        public string CurrentServerMessage => $"Currently Connected to: {(IsSimMonstersApiServer ? "Sim-Monsters Anti Cheat" : "Rigs of Rods")}";
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         public MainWindow()
         {
             DataContext = this;
             controller = new Controller();
-            GetCurrentServer();
             InitializeComponent();
         }
 
@@ -40,18 +26,10 @@ namespace SMAC
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        private void GetCurrentServer()
-        {
-            bool isSMApiServer = controller.settings.smApiServerActive;
-            CurrentServerMessage = $"Currently Connected to: {(isSMApiServer ? "Sim-Monsters Anti Cheat" : "Rigs of Rods")}";
-        }
-
         private void SetCurrentServer(bool isSMApiServer)
         {
-            if (controller.SwitchServers(isSMApiServer))
-            {
-                CurrentServerMessage = $"Currently Connected to: {(isSMApiServer ? "Sim-Monsters Anti Cheat" : "Rigs of Rods")}";
-            }
+            _ = controller.SwitchServers(isSMApiServer);
+            OnPropertyChanged(nameof(CurrentServerMessage));
         }
 
         private void btnSimMonsters_Click(object sender, RoutedEventArgs e)
